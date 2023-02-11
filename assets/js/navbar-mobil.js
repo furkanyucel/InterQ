e1775106ca990c5c726fcdcfcdc187ebfb4bff00
furkanyucel <furkanyucelux@gmail.com>
2023-02-11 19:46:49 +0300

    Header ( Navbar + Hero ) Updated

---
+++ assets/js/navbar-mobil.js
@@ -0,0 +1,9 @@
+function openMobilMenu() {
+  var x = document.querySelector(".navbar-mobil");
+  x.style.display = "block";
+}
+
+function closeMobilMenu() {
+  var x = document.querySelector(".navbar-mobil");
+  x.style.display = "none";
+}
